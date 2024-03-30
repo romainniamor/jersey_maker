795491d9db4e0df3e22d1151e031702cab9327c8
romainniamor <romainnavoret@gmail.com>
2024-03-30 18:23:27 +0100

    🚧 Add getEditTabsConfig

---
+++ client/src/config/getEditTabsConfig.ts
@@ -0,0 +1,31 @@
+import ai from "../assets/icons/ai.png";
+import logo from "../assets/icons/logo.png";
+import color from "../assets/icons/color.png";
+import brand from "../assets/icons/brand.png";
+
+export const getEditTabsConfig = (currentTabSelected) => [
+  {
+    index: "color",
+    label: "colorpicker",
+    icon: color,
+    className: currentTabSelected === "color" ? "active" : "",
+  },
+  {
+    index: "logo",
+    label: "logopicker",
+    icon: logo,
+    className: currentTabSelected === "logo" ? "active" : "",
+  },
+  {
+    index: "brand",
+    label: "brandpicker",
+    icon: brand,
+    className: currentTabSelected === "brand" ? "active" : "",
+  },
+  {
+    index: "ai",
+    label: "aipicker",
+    icon: ai,
+    className: currentTabSelected === "ai" ? "active" : "",
+  },
+];
