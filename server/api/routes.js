1c5616e9321194cd5168e8acbba6f81a78667b62
romainniamor <romainnavoret@gmail.com>
2024-04-02 15:29:22 +0200

    ✨ Server> add post to dalle

--- server/api/routes.js
+++ server/api/routes.js
@@ -1,12 +1,35 @@
 import express from "express";
 import dotenv from "dotenv";
+import OpenAI from "openai";
 
 const router = express.Router();
 
 dotenv.config();
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
-router.get("/create-jersey", (req, res) => {
-  res.json("Welcome to CREATE JERSEY API endpoints!");
+router.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to CREATE JERSEY API endpoints!" });
+});
+
+router.post("/dalle", async (req, res) => {
+  try {
+    const prompt = req.body.message;
+    const response = await openai.images.generate({
+      model: "dall-e-3",
+      prompt: prompt,
+      n: 1,
+      size: "1024x1024",
+      response_format: "b64_json",
+    });
+
+    const image = response.data[0].b64_json;
+    res.status(200).json({ image: image });
+  } catch (error) {
+    console.log("error /dalle post => ", error);
+    res.status(500).json({ message: `Error post dalle => ${error}` });
+  }
 });
 
 export default router;
--- server/index.js
+++ server/index.js
@@ -8,13 +8,13 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 const port = process.env.PORT;
 
-app.use("/api", routes);
+app.use("/api/create-jersey", routes);
 
 app.get("/", (req, res) => {
-  res.send("API is running...");
+  res.status(200).json("API is running...");
 });
 
 app.listen(port, () => {
