ec79174430e883afd49b25d91df5e66bb117ed55
romainniamor <romainnavoret@gmail.com>
2024-04-02 13:54:16 +0200

    🎉 🔧 Server config

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+// import routes from "./api/routes.js";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+const port = process.env.PORT;
+
+// app.use("/api", routes);
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
